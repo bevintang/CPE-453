0745fb98004aab5dc1dbaa3a65027391a3d784aa
Bevin Tang <bevintang@Bevins-MacBook-Air.local>
2018-04-14 16:48:04 -0700

    Completed defrag and free

--- programs/program1/malloc.c
+++ programs/program1/malloc.c
@@ -47,8 +47,6 @@ void* div16Ptr(void* ptr) {
  *
 **/
 void* canFit(size_t size) {
-	printf("Checking for requested size...\n");
-
 	/* Track the traversal of linked list */
 	Header* curHead;
 	size_t curHeadSize;
@@ -56,7 +54,6 @@ void* canFit(size_t size) {
 
 	/* Check if there are even any entries in the linked list */
 	if (linkedHeaders == NULL){
-		printf("\tLinked list is empty. Will append new header.\n");
 		return NULL;
 	}
 
@@ -73,20 +70,21 @@ void* canFit(size_t size) {
 
 	/* Exit while loop if proper size is found: */
 	if (curHeadSize >= size && isFree) {
-		printf("\tFound proper size!\n");
 		return curHead;
 	}
 	/* Or size not found (i.e. next Header is NULL) */
 	else{
-		printf("\tNo matches!\n");
 		return NULL;
 	}
 }
 
 /**
  *
- 	Retrieve that last node of the gloval linked list and return that
- 	node's address. Return NULL if the linkedlist is empty.
+ 	Retrieve that last node of the global linked list and return that
+ 	node's address. 
+
+ 	RETURN VALUE: Return a pointer to the address of the last Header. If linked
+ 				  list is empty, return NULL.
  *
 **/
 void* lastNode() {
@@ -107,7 +105,6 @@ void* lastNode() {
  *
 **/
 void append(Header* newHeader) {
-	printf("\tAppending new header...\n");
 	Header* tail = lastNode();
 	/* Check if the linked list is empty */
 	if (tail == NULL){
@@ -118,32 +115,122 @@ void append(Header* newHeader) {
 	}
 }
 
-void my_free(void* ptr) {
-	/* find address in Linked list that is immediately before the desired ptr */
-	Header* toFree = (Header*)ptr;
-	Header* previous = linkedHeaders;
-	Header* current = linkedHeaders;
-
-	while (current->next < toFree){
-		previous = current;
-		current = current->next;
-	}
-	previous->free = 1;
+int overFlow(size_t num1, size_t num2){
+	size_t sum = num1 + num2;
+	if (sum < num1 || sum < num2)
+		return 1;
+	return 0;
 }
 
+void combineHeaders(Header* current, Header* next){
+	/* Make current header hold new size */
+	current->size = current->size + next->size;
+
+	/* Make current's next equal to next's next */
+	if (next->next != NULL)
+		current->next = next->next;
+	else
+		current->next = NULL;
+}
+
+/**
+ *
+ 	Search through the linked list of Headers to combine any two adjacent, free
+ 	chunks. 
+
+ 	Performance: O(n).
+ *
+**/
+void defrag(){
+	Header* current = linkedHeaders;
+	size_t curSize = 0;
+	size_t nextSize = 0;
+	int curFree = 0;
+	int nextFree = 0;
+
+	/* Return if linked list isn't long enough */
+	if (current == NULL)
+		return;
+
+	/* Iterate through linked list and combine adjacent, free headers */
+	while (current->next != NULL){
+		curSize = current->size;
+		nextSize = current->next->size;
+		curFree = current->free;
+		nextFree = current->next->free;
+		if (curFree && nextFree && !overFlow(curSize, nextSize)){
+			printf("Defragging...\n");
+			combineHeaders(current, current->next);
+		}
+
+		if (current->next != NULL)
+			current = current->next;
+	}
+}
+
+/**
+ *
+ 	Given a size, create a new, non-free Header struct whose next param is NULL.
+
+ 	RETURN VALUE: Header struct pointer to the newly created Header.
+ *
+**/
+Header* newHeader(size_t size) {
+	Header* header;
+	size_t totalSize = size + sizeof(Header);
+
+	/* Ask for more memory using sbrk() */
+	header = sbrk(totalSize);
+
+	/* Construct this new header */
+	header->size = size;
+	header->free = 0;
+	header->next = NULL;
+
+	return header;
+}
+
+/**
+ *
+ 	Given a pointer, free the appropriate chunk of data that pointer is
+ 	a part of. To do this, find the header whose address is immediately
+ 	before the desired pointer, and free it.
+
+ 	At the end of the call, defrag the memory.
+ *
+**/
+void my_free(void* ptr) {
+	Header* toFree = (Header*)ptr;
+	Header* current = linkedHeaders;
+	Header* next = linkedHeaders;
+
+	if (current == NULL || next == NULL)
+		return;
+
+	while (next != NULL && current->next < toFree){
+		current = current->next;
+		next = current->next;
+	}
+	current->free = 1;
+	defrag();
+}
+
+/**
+ *
+ 	Check the linked list of headers to see if there is an available
+ 	size. If not, create a new header and set aside new memory with sbrk, and
+ 	append this new header to the linked list. Otherwise, set the appropriate
+ 	header's free flag to 0.
+
+ 	RETURN VALUE: A divisble-by-16 address that represents the location of the
+ 				  start of the data segment.
+ *
+**/
 void* my_malloc(size_t size) {
 	Header* header;
-	size_t size16 = size + sizeof(Header);
+	
 	if ((header = canFit(size)) == NULL){
-		/* Ask for more memory using sbrk() */
-		header = sbrk(size16);
-
-		/* Construct this new header */
-		header->size = size;
-		header->free = 0;
-		header->next = NULL;
-
-		/* add this new header to the linked list */
+		header = newHeader(size);
 		append(header);
 	}
 	else {
@@ -151,27 +238,34 @@ void* my_malloc(size_t size) {
 	}
 	printf("NEW HEADER: %p\n", header);
 	printf("DATA STARTS AT: %p\n\n\n", div16Ptr(header));
-	return div16Ptr(header);	/* return start of actual data */
+	return div16Ptr(header);
 }
 
+void printLinkedList(){
+	Header* current = linkedHeaders;
+	size_t i = 0;
+	while (current != NULL){
+		printf("%lu:\tSize: %lu\n\tFree: %d\n\tHeader: %p\n-----------------\n",
+			i++, current->size, current->free, current);
+		current = current->next;
+	}
+}
+
+/*
+	Crappy driver
+*/
 int main(void) {
 	int* sample1 = my_malloc(16);
 	int* sample2 = my_malloc(16);
-
-	/* These two addresses should be different */
-	printf("!!MAIN: Before free\n"); 
-	printf("!!MAIN: Sample1 %p\n", sample1);
-	printf("!!MAIN: Sample2 %p\n", sample2);
+	printLinkedList();
 
 	/* Remalloc sample1 */
 	my_free(sample1);
-	sample1 = my_malloc(160);
-	printf("!!MAIN: After free\n"); 
-	printf("!!MAIN: Sample1: %p\n", sample1);	/* Should be same address as original */
-
-	/* Add new headers to the end of linked list */
-	int* sample3 = my_malloc(65);
-	printf("!!MAIN: Sample3: %p\n\n\n", sample3);
-
+	printf("First free good\n");
+	my_free(sample2);
+	printf("Second free good\n");
+	sample1 = my_malloc(32);
+	sample2 = my_malloc(16);
+	printLinkedList();
 	return 0;
 }
