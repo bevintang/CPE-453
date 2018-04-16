f9a1a130469261dc5e3af890a0c9b6cadf9d754a
Bevin Tang <bevintang@Bevins-MacBook-Air.local>
2018-04-15 17:28:20 -0700

    Created new test cases for realloc

--- programs/program1/main.c
+++ programs/program1/main.c
@@ -5,54 +5,58 @@
 	Crappy driver
 */
 int main(void) {
-	int* sample1 = (int*)malloc(16);
+	int* sample1 = (int*)realloc(NULL, 16);
 	printLinkedList();
-	char* sample2 = (char*)malloc(160);
+	char* sample2 = (char*)realloc(NULL, 160);
 	printLinkedList();
 
 	/* Remalloc sample1 */	
 	printf("FREEING SAMPLE1!...\n");
-	free(sample1);
+	sample1 = realloc(sample1, 0);
 	printLinkedList();
 
 	printf("MALLOCING SAMPLE1 16...\n");
-	sample1 = malloc(16);
+	sample1 = realloc(NULL, 16);
 	printLinkedList();
 
 	printf("FREEING SAMPLE2!...\n");
-	free(sample2);
+	sample2 = realloc(sample2, 0);
 	printLinkedList();
 
 	printf("MALLOCING SAMPLE2 4...\n");
-	sample2 = malloc(4);
+	sample2 = realloc(NULL, 4);
 	printLinkedList();
 
-	printf("CALLOCING SAMPLE3 64...\n");
-	int* sample3 = calloc(16, 4);
+	printf("REALLOCING SAMPLE3 64...\n");
+	int* sample3 = realloc(NULL, 64);
 	printLinkedList();
 
-	printf("CALLOCING SAMPLE4 160...\n");
-	int* sample4 = calloc(16, 10);
+	printf("REALLOCING SAMPLE4 160...\n");
+	int* sample4 = realloc(NULL, 160);
 	printLinkedList();
 
-	printf("\n\nFREEING SAMPLE2 4\n");
-	free(sample2);
+	printf("\n\nREALLOCING SAMPLE2 4 -> 64\n");
+	sample2 = realloc(sample2, 64);
 	printLinkedList();
 
-	printf("\n\nFREEING SAMPLE3 64\n");
-	free(sample3);
+	printf("\n\nREALLOCING SAMPLE3 64 -> 16\n");
+	sample3 = realloc(sample3, 16);
 	printLinkedList();
 
 	printf("\n\nFREEING SAMPLE4 160\n");
-	free(sample4 + 10);
+	sample4 = realloc(sample4 + 10, 0);
 	printLinkedList();
 
 	printf("\n\nREALLOC SAMPLE1 160\n");
 	sample1 = realloc(sample1, 160);
 	printLinkedList();
 
-	printf("\n\nREALLOC nonexisting pointer 200\n");
+	printf("\n\nREALLOC SAMPLE1 pointer 160 -> 20000\n");
 	sample1 = realloc(sample1, 20000);
+	printLinkedList();
+
+	printf("\n\nREALLOC SAMPLE1 pointer 20000 -> 16\n");
+	sample1 = realloc(sample1, 16);
 	printLinkedList();
 
 	return 0;
