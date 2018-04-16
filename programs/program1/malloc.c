ba9110dda39daa62d74201bab3707a777fa45b5f
Bevin Tang <bevintang@bevins-air.home>
2018-04-15 22:30:42 -0700

    Upper bounds for size

--- programs/program1/malloc.c
+++ programs/program1/malloc.c
@@ -64,6 +64,8 @@ Header* newHeader(size_t size) {
  *
 **/
 int insertHeader(Header* current, size_t size) {
+	if (size > (size_t)current->next)
+		return 0;
 	/* Construct new header */
 	Header* newHeader = (Header*)((size_t)current + current->size);
 	void* newEndOfData = (void*)((size_t)newHeader + div16(sizeof(Header)) + size);
