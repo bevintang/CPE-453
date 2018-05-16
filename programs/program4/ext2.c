7f14335dc89ef4e864b8afcf7d82e1abc15c571c
bevintang <bevintang@gmail.com>
2018-05-16 14:16:44 -0700

    Added Makefile and empty main src file, program4.c

--- programs/program4/ext2.c
+++ programs/program4/ext2.c
@@ -1,5 +1,9 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 //the block argument is in terms of SD card 512 byte sectors
-void read_data(uint32_t block, uint16_t offset, uint8_t* data, uint16_t size) {
+void read_data(uint32_t block, uint16_t offset, uint8_t* data, uint16_t size, FILE* fp) {
    if (offset > 511) {
       printf ("Offset greater than 511.\n");
       exit(0);
@@ -8,3 +12,4 @@ void read_data(uint32_t block, uint16_t offset, uint8_t* data, uint16_t size) {
    fseek(fp,block*512 + offset,SEEK_SET);
    fread(data,size,1,fp);
 }
+
